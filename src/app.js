52b6c6a4b2f52deb41cda20422e4e4e6fbda8901
Cédric Finance <cedric.finance@finandric.com>
2014-12-19 18:27:02 +0100

    Generate random data for the metrics

--- src/app.js
+++ src/app.js
@@ -7,6 +7,11 @@ var port = 61613;
 
 var client = new Stomp('mq', port, user, pass);
 
+function random(minValue, maxValue) {
+  var rand = Math.random();
+  return Math.floor(rand * (maxValue - minValue) + minValue);
+}
+
 client.connect(function(sessionId) {
     client.subscribe(destination, function(body, headers) {
       console.log('Message received:', body);
@@ -16,7 +21,10 @@ client.connect(function(sessionId) {
 
     function publish() {
       client.publish(destination, JSON.stringify(metrics));
-      metrics.heurespleines++;
+      metrics.heurescreuses += random(1, 10);
+      metrics.heurespleines += random(1, 10);
+      metrics.watts = Math.max(0, metrics.watts + random(-100, 100));
+      metrics.amperes = Math.max(0, metrics.amperes + random(-3, 3));
     }
     setInterval(publish, 1000);
 });
