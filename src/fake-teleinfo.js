76844f3cd0f0f8a4ae91e385b88b537549e1b7b9
Cédric Finance <cedric.finance@finandric.com>
2014-12-27 21:19:05 +0100

    Add the pricing period and take it into account when generating fake data

--- src/fake-teleinfo.js
+++ src/fake-teleinfo.js
@@ -6,8 +6,13 @@ function random(minValue, maxValue) {
 }
 
 function nextFrame(frame) {
-  frame.HCHC += random(1, 10);
-  frame.HCHP += random(1, 10);
+  frame._index++;
+  frame.PTEC = ["HC..", "HP.."][Math.floor(frame._index/20)%2];
+  if (frame.PTEC==="HC..") {
+    frame.HCHC += random(1, 10);
+  } else {
+    frame.HCHP += random(1, 10);
+  }
   frame.PAPP = Math.max(0, frame.PAPP + random(-100, 100));
   frame.IINST = Math.max(0, frame.IINST + random(-3, 3));
 }
@@ -20,7 +25,7 @@ function publish(emitter, frame) {
 module.exports = function() {
   var frameEvents = new events.EventEmitter();
 
-  var frame = { PAPP: 340, IINST: 1, HCHC: 10000000, HCHP: 50000000 };
+  var frame = { PAPP: 340, IINST: 1, HCHC: 10000000, HCHP: 50000000, _index: 0 };
 
   setInterval(publish, 1000, frameEvents, frame);
 
